530f793602026aa60c192d5b0198aee8a10ed387
gagan-kotian <142842464+gagan-kotian@users.noreply.github.com>
2024-09-24 13:37:32 +0530

    H

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-2d7w)*^p*=)p56qfl$j7jtxv7rwhc^5c36k-(omg^bc$fk8_t+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.vercel.app',  # Allows all subdomains of vercel.app
+    'localhost',
+    '127.0.0.1'
+]
+
 
 
 # Application definition
